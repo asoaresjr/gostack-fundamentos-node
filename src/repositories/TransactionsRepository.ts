1fa3a5e088b2bded5d000ad18b509715f91e9808
asoares <asoares@vtinova.com.br>
2020-06-26 22:50:43 -0400

    create, list and balance

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTranasctionDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -15,14 +21,35 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    //
+    const income = this.transactions.reduce((accumulator, transaction) => {
+      return transaction.type === 'income'
+        ? accumulator + transaction.value
+        : accumulator;
+    }, 0);
+
+    const outcome = this.transactions.reduce((accumulator, transaction) => {
+      return transaction.type === 'outcome'
+        ? accumulator + transaction.value
+        : accumulator;
+    }, 0);
+
+    const total = income - outcome;
+
+    return { total, income, outcome };
   }
 
-  public create(): Transaction {
+  public create({ title, value, type }: CreateTranasctionDTO): Transaction {
     // TODO
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
